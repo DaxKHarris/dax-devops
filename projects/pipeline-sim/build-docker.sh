0236d248444232f91a15551196f649ddcf0fbc25
SchrodingersKage <DaxKHarris@gmail.com>
2025-05-21 00:06:30 -0600

    Added docker prune to docker build file.

--- projects/pipeline-sim/build-docker.sh
+++ projects/pipeline-sim/build-docker.sh
@@ -4,3 +4,5 @@ cd ~/Documents/Git/dax-devops/projects/pipeline-sim && sudo docker build -t pipe
 echo "Dockerfile built!"
 
 sudo docker run --rm -it -v "$HOME/.ssh:/root/.ssh:ro" -v "$(pwd)/../..:/pipeline" pipeline-image
+
+sudo docker image prune
