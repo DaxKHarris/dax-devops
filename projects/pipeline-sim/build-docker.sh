d8a153dc5cf5df877f33b7530ddcfff80bf9f548
SchrodingersKage <DaxKHarris@gmail.com>
2025-05-20 23:11:15 -0600

    Created sh for automatic docker build

---
+++ projects/pipeline-sim/build-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~/Documents/Git/dax-devops/projects/pipeline-sim && sudo docker build -t pipeline-image .
+echo "Dockerfile built!"
+
+sudo docker run --rm -v "$HOME/.ssh:/root/.ssh:ro" \
+-v "$(pwd)/../..:pipeline" pipeline-image
